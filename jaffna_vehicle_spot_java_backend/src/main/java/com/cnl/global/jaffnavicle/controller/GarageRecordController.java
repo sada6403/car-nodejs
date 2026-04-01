149386d7b6020c5008e8fb2b03a85a6c528afba1
Lavapperiyan <lavapperiyan@example.com>
2026-04-01 23:46:43 +0530

    Consolidate Node.js, Java, and Flutter Frontend into one repository

--- jaffna_vehicle_spot_java_backend/src/main/java/com/cnl/global/jaffnavicle/controller/GarageRecordController.java
+++ jaffna_vehicle_spot_java_backend/src/main/java/com/cnl/global/jaffnavicle/controller/GarageRecordController.java
@@ -3,7 +3,6 @@ package com.cnl.global.jaffnavicle.controller;
 import com.cnl.global.jaffnavicle.model.GarageRecord;
 import com.cnl.global.jaffnavicle.repository.GarageRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
